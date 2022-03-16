3255f27949ff3dce2a3ee7a730f21801e4e53b46
Joe <zvmboni@gmail.com>
2022-03-16 10:04:43 -0700

    commit tree 6cbfefa7da8b939eb10e19486774e2b121205b3f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names){
+    const messages = [...names];
+    for (let i=0; i<messages.length; i++){
+        messages[i] = `Thank you, ${messages[i]}, for the wonderful surprise gift!`;
+    }
+
+    return messages;
+}
+
+function countDown(number){
+    for (let i=number; i>-1; i--){
+        console.log(i);
+    }
+}
